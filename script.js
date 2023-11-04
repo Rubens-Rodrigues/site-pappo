09ba2e55934666f039ac1d24a51a4a4855185f62
rubens-rodrigues <misterubens@gmail.com>
2023-11-04 11:01:12 -0300

    Att

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-menu");
+const topoIcons = document.querySelector(".topo-icons")
 
 menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-    menuToggle.classList.toggle("active");
+	navLinks.classList.toggle("active");
+	menuToggle.classList.toggle("active");
+
+	if (navLinks.classList.contains("active")) {
+		topoIcons.style.display = "none";
+	} else {
+		topoIcons.style.display = "";
+	}
 });
