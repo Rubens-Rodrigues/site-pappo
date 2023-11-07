1587e20d9626c9f0b76bce3a6d1e561d38bb28aa
rubens-rodrigues <misterubens@gmail.com>
2023-11-07 16:51:52 -0300

    Att Mobille

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-menu");
-const topoIcons = document.querySelector(".topo-icons")
+const topoIcons = document.querySelector(".topo-icons");
 
 menuToggle.addEventListener("click", () => {
 	navLinks.classList.toggle("active");
@@ -11,4 +11,43 @@ menuToggle.addEventListener("click", () => {
 	} else {
 		topoIcons.style.display = "";
 	}
+});
+
+
+//slider blog
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider");
+  const slides = document.querySelectorAll(".slide");
+  const prevBtn = document.querySelector(".prev");
+  const nextBtn = document.querySelector(".next");
+  let currentIndex = 0;
+  let slidesToShow = 1;
+
+  const slideWidth = slides[0].offsetWidth;
+
+  function prevSlide() {
+    if (currentIndex > 0) {
+      currentIndex -= slidesToShow;
+      if (currentIndex < 0) {
+        currentIndex = 0;
+      }
+      updateSlider();
+    }
+  }
+
+  function nextSlide() {
+    if (currentIndex < slides.length - slidesToShow) {
+      currentIndex += slidesToShow;
+      updateSlider();
+    }
+  }
+
+  function updateSlider() {
+    const translateX = -currentIndex * slideWidth;
+    slider.style.transform = `translateX(${translateX}px)`;
+  }
+
+  prevBtn.addEventListener("click", prevSlide);
+  nextBtn.addEventListener("click", nextSlide);
 });
